9b4654fb6bdaeedc924894e2e91c735f5200b1f4
Anthony J. Clark <anthonyjclark@gmail.com>
2014-11-18 16:15:36 -0500

    Added more cv data, a Handlebars template, a simple flex-based layout style, and 2 test html files.

--- json2cv.js
+++ json2cv.js
@@ -1,5 +1,28 @@
 
+
+var Template = require('template');
+var template = new Template();
+
+var Consolidate = require('consolidate');
+template.engine('hbs', Consolidate.handlebars);
+
+
+// Load the CV template
+template.page('cv.hbs');
+
+
+// Open the json file (CL argument)
 var fs = require('fs');
 var cvdata = JSON.parse(fs.readFileSync(process.argv[2], 'utf8'));
 
-console.log(cvdata);
+
+// Render the page
+var template_data = {
+  header: cvdata.header,
+  sections: cvdata.sections
+};
+template.render('cv.hbs', template_data, function(err, html) {
+  if (err) throw err;
+  console.log(html);
+});
+
